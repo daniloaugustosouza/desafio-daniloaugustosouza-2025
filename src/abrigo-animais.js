bd17a37f5741117383e276ac8031baec054b736d
Danilo Augusto <souzadanilo6augusto@gmail.com>
2025-09-07 19:38:22 -0400

    List of Animals

--- src/abrigo-animais.js
+++ src/abrigo-animais.js
@@ -1,6 +1,19 @@
 class AbrigoAnimais {
 
   encontraPessoas(brinquedosPessoa1, brinquedosPessoa2, ordemAnimais) {
+	const animaisCertos ={
+      	Rex:{raca: 'cão', brinquedos: ['RATO','BOLA']},
+      	Mimi:{raca: 'gato', brinquedos: ['BOLA','LASER']},
+      	Fofo:{raca: 'gato', brinquedos: ['BOLA','RATO','LASER']},
+      	Zero:{raca: 'gato', brinquedos: ['RATO','BOLA']},
+      	Bola:{raca: 'cão', brinquedos: ['CAIXA','NOVELO']},
+      	Bebe:{raca: 'cão', brinquedos: ['LASER','RATO','BOLA']},
+      	Loco:{raca: 'jabuti', brinquedos: ['SKATE','RATO']},
+    };
+
+
+	
+	return {lista: []};
   }
 }
 
